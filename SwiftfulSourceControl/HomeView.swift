bb4aa55dd5f3bc6001da8327611a0d18d1906ca1
Zachary <Zachary.Haft@icloud.com>
2026-06-19 09:29:00 -0400

    Add HomeView

---
+++ SwiftfulSourceControl/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  SwiftfulSourceControl
+//
+//  Created by Zachary Haft on 6/19/26.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("Screen 2!")
+    }
+}
+
+#Preview {
+    HomeView()
+}
