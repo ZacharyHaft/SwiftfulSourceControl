aad542c882d1ca8b1872aef40c8d1dda1e14344c
Zachary <Zachary.Haft@icloud.com>
2026-06-19 09:39:09 -0400

    [Feature] Add subscribe button

--- SwiftfulSourceControl/ContentView.swift
+++ SwiftfulSourceControl/ContentView.swift
@@ -38,6 +38,10 @@ struct ContentView: View {
             Button("Click me") {
                 
             }
+            
+            Button("Subscribe") {
+                
+            }
         }
         .padding()
     }
