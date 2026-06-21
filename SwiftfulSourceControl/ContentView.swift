1503d4ad74c848eb15f2e2aa8ff5fb0869a0612d
Zachary <Zachary.Haft@icloud.com>
2026-06-21 05:39:05 -0400

    Commit message

--- SwiftfulSourceControl/ContentView.swift
+++ SwiftfulSourceControl/ContentView.swift
@@ -54,7 +54,7 @@ struct ContentView: View {
                             
                        
                             
-                            Button("Subscribe now!") {
+                            Button("Subscribe!") {
                                 
                             }
                             
