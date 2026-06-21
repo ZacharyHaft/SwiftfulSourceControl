4ea3e7c765afb9ff06915b7f6e6ea5c8469a9442
Zachary <Zachary.Haft@icloud.com>
2026-06-21 05:25:56 -0400

    [Feature] Add SettingsView

---
+++ SwiftfulSourceControl/SettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsView.swift
+//  SwiftfulSourceControl
+//
+//  Created by Zachary Haft on 6/21/26.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text("Settings!")
+    }
+}
+
+#Preview {
+    SettingsView()
+}
